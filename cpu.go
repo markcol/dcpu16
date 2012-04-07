96f4e23778a1e6de4b16c242ad1e38d0c3429287
Mark Colburn <markcol@mac.com>
2012-04-06 23:55:53 -0700

    Convert functional CPU into an object with threadsafe state access.
    By converting to an object, it allows multiple CPUs to be created and allows
    for a CPU to run in it's own goroutine. The state access functions are
    guaranteed to run only at instruction boundaries, ensuring that state updated
    or returned is atomic with respect to the virtual machine cyle.

--- cpu.go
+++ cpu.go
@@ -2,6 +2,7 @@ package dcpu16
 
 import (
 	"math"
+	"sync"
 )
 
 const (
@@ -19,101 +20,118 @@ const (
 	Z
 	I
 	J
-	O       // overflow
-	PC      // program counter
-	SP      // stack pointern
-	TICK    // clock ticks (cycle counter)
-	regSize // number of registers (for sizing register slice)
+	// The following registers are exported in the Register call
+	O
+	SP
+	PC
+	TICK
+	regSize = iota // number of registers (for sizing register slice)
 )
 
-var (
-	register = make([]uint16, regSize)
-	memory   = make([]uint16, RAMSIZE)
-	sp       = &register[SP]
-	pc       = &register[PC]
-	cycles   = &register[TICK]
-)
+// DCPU16 is a single virtual CPU that conforms to the 0x10c.com dcpu16 spec.
+// The CPU can be run in a separate goroutine. The state access functions
+// (Read, Write, Registers, etc.) will only be executed at instruction boundaries,
+// ensuring that the state returned is consistent and atomic with respect to
+// the virtual CPU.
+type DCPU16 struct {
+	register [8]uint16
+	memory   [RAMSIZE]uint16
+	overflow uint16
+	sp       uint16
+	pc       uint16
+	tick     uint16
+	mutex    sync.Mutex
+}
 
 // Write writes the words from the slice data into memory starting at the
 // address in addr. Any existing data will be overwritten.
-//
 // If addr + len(data) > MEMSIZE, only MEMSIZE-addr+1 words will be copied.
-func Write(addr uint16, data []uint16) {
-	copy(memory[addr:], data)
+func (c *DCPU16) Write(addr uint16, data []uint16) {
+	// wait for an instruction boundary
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	copy(c.memory[addr:], data)
 }
 
 // Read reads (at most) len words from memory starting at the given address and
 // returns them to the caller. The number of words returned may be less than
 // requested if address+len exeeds addressable memory.
-func Read(addr uint16, l int) []uint16 {
+func (c *DCPU16) Read(addr uint16, l int) []uint16 {
+	// wait for an instruction boundary
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
 	if int(addr)+l > LASTADDR {
 		l = LASTADDR - int(addr) + 1
 	}
 	d := make([]uint16, l)
-	copy(d, memory[addr:])
+	copy(d, c.memory[addr:])
 	return d
 }
 
 // Registers returns a slice of words that contains the value of the current
 // CPU registers. The registers are stored in the following order: a, b, c, x,
-// y, z, i, j, o, pc, sp, tick.
-func Registers() []uint16 {
+// y, z, i, j, o, sp, pc, tick.
+func (c *DCPU16) Registers() []uint16 {
+	// wait for an instruction boundary
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
 	r := make([]uint16, regSize)
-	copy(r, register)
+	copy(r, c.register[:])
+	r[O] = c.overflow
+	r[PC] = c.pc
+	r[SP] = c.sp
+	r[TICK] = c.tick
 	return r
 }
 
-// Reset sets all registers and memory to zero
-func Reset() {
-	for i := range register {
-		register[i] = 0
-	}
-	for i := range memory {
-		memory[i] = 0
-	}
-}
-
 // Step executes a single instruction and returns to the caller.
-func Step() {
-	step()
+func (c *DCPU16) Step() {
+	c.step()
 }
 
 // Run executes instructions endlessly.
-func Run() {
+func (c *DCPU16) Run() {
 	for true {
-		step()
+		c.step()
 	}
 }
 
 // step executes a single machine instruction at [pc], updating all registers,
 // memory, and cycle counts.
-func step() {
+func (c *DCPU16) step() {
 	var opcode uint16
 
+	// hold lock during entire instruction cycle
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
 	// read the opcode
-	opcode = nextWord()
+	opcode = c.nextWord()
 
 	// check for extended opcode
 	if (opcode & 0x0f) == 0 {
-		extended(opcode)
+		c.extended(opcode)
 	} else {
-		standard(opcode)
+		c.standard(opcode)
 	}
 
 	// calculate the cycle count. NB: count is 1 less then spec, since nextWork
 	// adds 1 to count during the opcode fetch.
 	switch opcode & 0x0f {
 	case 0x5, 0x6:
-		(*cycles) += 2
+		c.tick += 2
 	case 0x1, 0x9, 0xa, 0xb:
 		break
 	default:
-		(*cycles) += 1
+		c.tick++
 	}
 }
 
 // standard executes single a (non-extended) instruction opcode.
-func standard(opcode uint16) {
+func (c *DCPU16) standard(opcode uint16) {
 	var (
 		a, b       *uint16
 		aval, bval uint16
@@ -121,8 +139,8 @@ func standard(opcode uint16) {
 	)
 
 	// fetch and evaluate a, then b
-	a = lea((opcode&0x3f0)>>4, &aval)
-	b = lea((opcode&0xfc00)>>10, &bval)
+	a = c.lea((opcode&0x3f0)>>4, &aval)
+	b = c.lea((opcode&0xfc00)>>10, &bval)
 
 	// "If any instruction tries to assign a literal value, the assignment
 	// fails silently. Other than that, the instruction behaves as normal."
@@ -136,28 +154,28 @@ func standard(opcode uint16) {
 	case 0x2: // ADD a, b - sets a to a+b, sets O to 0x0001 if there's an overflow, 0x0 otherwise
 		v = int(*a) + int(*b)
 		if v > math.MaxInt16 {
-			register[O] = 1
+			c.overflow = 1
 		} else {
-			register[O] = 0
+			c.overflow = 0
 		}
 		*a = uint16(v)
 	case 0x3: // SUB a, b - sets a to a-b, sets O to 0xffff if there's an underflow, 0x0 otherwise
 		v = int(*a) - int(*b)
 		if v < math.MinInt16 {
-			register[O] = 1
+			c.overflow = 1
 		} else {
-			register[O] = 0
+			c.overflow = 0
 		}
 		*a = uint16(v)
 	case 0x4: // MUL a, b - sets a to a*b, sets O to ((a*b)>>16)&0xffff
-		register[O] = ((*a * *b) >> 16) & 0xffff
+		c.overflow = uint16(((int(*a) * int(*b)) >> 16) & 0xffff)
 		*a *= *b
 	case 0x5: // DIV a, b - sets a to a/b, sets O to ((a<<16)/b)&0xffff. if b==0, sets a and O to 0 instead.
 		if *b == 0 {
 			*a = 0
-			register[O] = 0
+			c.overflow = 0
 		} else {
-			register[O] = ((*a << 16) / *b) & 0xffff
+			c.overflow = uint16(((int(*a) << 16) / int(*b)) & 0xffff)
 			*a /= *b
 		}
 	case 0x6: // MOD a, b - sets a to a%b. if b==0, sets a to 0 instead.
@@ -168,10 +186,10 @@ func standard(opcode uint16) {
 		}
 	case 0x7: // SHL a, b - sets a to a<<b, sets O to ((a<<b)>>16)&0xffff
 		*a <<= *b
-		register[O] = ((*a << *b) >> 16) & 0x0ffff
+		c.overflow = ((*a << *b) >> 16) & 0x0ffff
 	case 0x8: // SHR a, b - sets a to a>>b, sets O to ((a<<16)>>b)&0xffff
 		*a >>= *b
-		register[O] = ((*a << *b) >> 16) & 0x0ffff
+		c.overflow = ((*a << *b) >> 16) & 0x0ffff
 	case 0x9: // AND a, b - sets a to a&b
 		*a &= *b
 	case 0xa: // BOR a, b - sets a to a|b
@@ -180,35 +198,35 @@ func standard(opcode uint16) {
 		*a ^= *b
 	case 0xc: // IFE a, b - performs next instruction only if a==b
 		if !(*a == *b) {
-			nextWord()
+			c.nextWord()
 		}
 	case 0xd: // IFN a, b - performs next instruction only if a!=b
 		if !(*a != *b) {
-			nextWord()
+			c.nextWord()
 		}
 	case 0xe: // IFG a, b - performs next instruction only if a>b
 		if !(*a > *b) {
-			nextWord()
+			c.nextWord()
 		}
 	case 0xf: // IFB a, b - performs next instruction only if (a&b)!=0
 		if !((*a & *b) != 0) {
-			nextWord()
+			c.nextWord()
 		}
 	}
 }
 
 // extnded executes a single extended instruction opcode.
-func extended(opcode uint16) {
+func (c *DCPU16) extended(opcode uint16) {
 	var (
 		a    *uint16
 		aval uint16
 	)
 
-	a = lea((opcode&0xfc00)>>10, &aval)
+	a = c.lea((opcode&0xfc00)>>10, &aval)
 	switch (opcode & 0x3f) >> 4 {
 	case 0x1: // JSR a
-		pushValue(*pc)
-		*pc = *a
+		c.pushValue(c.pc)
+		c.pc = *a
 	default:
 		panic("Invalid extended opcode")
 	}
@@ -219,33 +237,33 @@ func extended(opcode uint16) {
 //
 // Note this function returns a host pointer to guest memory, register, or
 // a host-provided constant buffer.
-func lea(addr uint16, cval *uint16) *uint16 {
+func (c *DCPU16) lea(addr uint16, cval *uint16) *uint16 {
 	switch {
 	case addr <= 0x07: // register
-		return &register[addr]
+		return &c.register[addr]
 	case addr <= 0x0f: // [register]
-		return &memory[register[addr-0x08]]
+		return &c.memory[c.register[addr-0x08]]
 	case addr <= 0x17: // [next word + register]
-		(*cycles)++
-		return &memory[nextWord()+register[addr-0x10]]
+		c.tick++
+		return &c.memory[c.nextWord()+c.register[addr-0x10]]
 	case addr == 0x18: // POP
-		return pop()
+		return c.pop()
 	case addr == 0x19: // PEEK
-		return &memory[*sp]
+		return &c.memory[c.sp]
 	case addr == 0x1a: // PUSH
-		return push()
+		return c.push()
 	case addr == 0x1b: // SP
-		return sp
+		return &c.sp
 	case addr == 0x1c: // PC
-		return pc
+		return &c.pc
 	case addr == 0x1d: // O (overflow register)
-		return &register[O]
+		return &c.overflow
 	case addr == 0x1e: // [next word]
-		(*cycles)++
-		return &memory[nextWord()]
+		c.tick++
+		return &c.memory[c.nextWord()]
 	case addr == 0x1f: // next word (literal)
-		(*cycles)++
-		*cval = nextWord()
+		c.tick++
+		*cval = c.nextWord()
 		return cval
 	case addr <= 0x3f: // literal value 0x00-0x1f (literal)
 		*cval = addr - 0x20
@@ -255,31 +273,31 @@ func lea(addr uint16, cval *uint16) *uint16 {
 }
 
 // nextWord returns the value of the memory at [pc] and increments the pc.
-func nextWord() (v uint16) {
-	v = memory[*pc]
-	(*pc)++
-	(*cycles)++
+func (c *DCPU16) nextWord() (v uint16) {
+	v = c.memory[c.pc]
+	c.pc++
+	c.tick++
 	return
 }
 
 // push returns the value &[--sp]
 // Note: returns a host pointer to the guest memory.
-func push() (v *uint16) {
-	(*sp)--
-	v = &memory[*sp]
+func (c *DCPU16) push() (v *uint16) {
+	c.sp--
+	v = &c.memory[c.sp]
 	return
 }
 
 // pushValue pushes the word val onto the stack.
-func pushValue(val uint16) {
-	(*sp)--
-	memory[*sp] = val
+func (c *DCPU16) pushValue(val uint16) {
+	c.sp--
+	c.memory[c.sp] = val
 }
 
 // pop returns the value &[sp++]
 // Note: returns a host pointer to the guest memory.
-func pop() (v *uint16) {
-	v = &memory[*sp]
-	(*sp)++
+func (c *DCPU16) pop() (v *uint16) {
+	v = &c.memory[c.sp]
+	c.sp++
 	return
 }
--- cpu_test.go
+++ cpu_test.go
@@ -3,176 +3,204 @@ package dcpu16
 import "testing"
 
 func TestWriteAndRead(t *testing.T) {
-	Write(0, []uint16{0x7c01, 0x0030, 0x7de1})
-	if memory[0] != 0x7c01 && memory[1] != 0x0030 && memory[2] != 0x7de1 {
-		t.Errorf("Expected memory to equal written values, got: %v\n", memory[0:4])
+	c := new(DCPU16)
+	c.Write(0, []uint16{0x7c01, 0x0030, 0x7de1})
+	if c.memory[0] != 0x7c01 && c.memory[1] != 0x0030 && c.memory[2] != 0x7de1 {
+		t.Errorf("Expected memory to equal written values, got: %v\n", c.memory[0:4])
 	}
-	m := Read(0, 4)
+	m := c.Read(0, 4)
 	if m[0] != 0x7c01 && m[1] != 0x0030 && m[2] != 0x7de1 {
 		t.Errorf("Expected memory to equal written values, got: %v\n", m)
 	}
 }
 
 func TestRegisters(t *testing.T) {
-	Reset()
+	c := new(DCPU16)
 	// expect the registers to be zeroed
 	e := make([]uint16, regSize)
-	checkRegisters(e, t)
+	checkRegisters(e, c, t)
 }
 
 func TestSetA(t *testing.T) {
-	Reset()
-	Write(0, []uint16{0x7c01, 0x0030}) // SET A, 0x30
-	e := Registers()
+	c := new(DCPU16)
+	c.Write(0, []uint16{0x7c01, 0x0030}) // SET A, 0x30
+	e := c.Registers()
 	e[PC] += 2
 	e[TICK] += 3
 	e[A] = 0x30
-	step()
-	checkRegisters(e, t)
+	c.step()
+	checkRegisters(e, c, t)
 }
 
 func TestSetAllRegisters(t *testing.T) {
+	c := new(DCPU16)
 	for i := 0; i <= 7; i++ {
-		Reset()
-		memory[0] = makeOpcode(0x01, i, 0x1f) // SET I, 0x0030
-		memory[1] = 0x0030
-		e := Registers()
-		e[PC] += 2
+		c.memory[0] = makeOpcode(0x01, i, 0x1f) // SET I, 0x0030
+		c.memory[1] = 0x0030
+		c.pc = 0
+		e := c.Registers()
+		e[PC] = 2
 		e[TICK] += 3
 		e[i] = 0x30
-		step()
-		checkRegisters(e, t)
+		c.step()
+		checkRegisters(e, c, t)
 	}
 }
 
 func TestSetPC(t *testing.T) {
-	Reset()
-	Write(0, []uint16{0x7dc1, 0x0030}) // SET PC, 0x30
-	e := Registers()
+	c := new(DCPU16)
+	c.Write(0, []uint16{0x7dc1, 0x0030}) // SET PC, 0x30
+	e := c.Registers()
 	e[PC] = 0x30
 	e[TICK] += 3
-	step()
-	checkRegisters(e, t)
+	c.step()
+	checkRegisters(e, c, t)
 }
 
 func TestSetO(t *testing.T) {
-	Reset()
-	Write(0, []uint16{0x7dd1, 0x0030}) // SET O, 0x30
-	e := Registers()
+	c := new(DCPU16)
+	c.Write(0, []uint16{0x7dd1, 0x0030}) // SET O, 0x30
+	e := c.Registers()
 	e[O] = 0x30
 	e[PC] += 2
 	e[TICK] += 3
-	step()
-	checkRegisters(e, t)
+	c.step()
+	checkRegisters(e, c, t)
 }
 
 func TestSetSP(t *testing.T) {
-	Reset()
-	Write(0, []uint16{0x7db1, 0x0030}) // SET SP, 0x30
-	e := Registers()
+	c := new(DCPU16)
+	c.Write(0, []uint16{0x7db1, 0x0030}) // SET SP, 0x30
+	e := c.Registers()
 	e[SP] = 0x30
 	e[PC] += 2
 	e[TICK] += 3
-	step()
-	checkRegisters(e, t)
+	c.step()
+	checkRegisters(e, c, t)
 }
 
 func TestSetRegisterIndirect(t *testing.T) {
-	Reset()
-	Write(0, []uint16{0x2811, 0xabca}) // SET B, [C]
-	register[C] = 1
-	e := Registers()
-	e[B] = memory[1]
+	c := new(DCPU16)
+	c.Write(0, []uint16{0x2811, 0xabca}) // SET B, [C]
+	c.register[C] = 1
+	e := c.Registers()
+	e[B] = c.memory[1]
 	e[C] = 1
 	e[PC] += 1
 	e[TICK] += 1
-	step()
-	checkRegisters(e, t)
+	c.step()
+	checkRegisters(e, c, t)
 }
 
 func TestSetRegisterIndirectOffset(t *testing.T) {
-	Reset()
-	Write(0, []uint16{0x4011, 0x0000}) // SET B, [0x0000+A]
-	e := Registers()
-	e[B] = memory[0]
+	c := new(DCPU16)
+	c.Write(0, []uint16{0x4011, 0x0000}) // SET B, [0x0000+A]
+	e := c.Registers()
+	e[B] = c.memory[0]
 	e[PC] += 2
 	e[TICK] += 3
-	step()
-	checkRegisters(e, t)
+	c.step()
+	checkRegisters(e, c, t)
 }
 
 func TestSetIndirect(t *testing.T) {
-	Reset()
-	Write(0, []uint16{0x7811, 0x0002, 0x7ce3}) // SET B, [0x0002]
-	e := Registers()
-	e[B] = memory[2]
+	c := new(DCPU16)
+	c.Write(0, []uint16{0x7811, 0x0002, 0x7ce3}) // SET B, [0x0002]
+	e := c.Registers()
+	e[B] = c.memory[2]
 	e[PC] += 2
 	e[TICK] += 3
-	step()
-	checkRegisters(e, t)
+	c.step()
+	checkRegisters(e, c, t)
 }
 
 func TestSetAllShortLiterals(t *testing.T) {
+	c := new(DCPU16)
 	for i := 0; i <= 0x1f; i++ {
-		Reset()
-		memory[0] = makeOpcode(0x01, 0, 0x20+i) // SET A, i
-		e := Registers()
-		e[PC] += 1
+		c.pc = 0
+		c.memory[0] = makeOpcode(0x01, 0, 0x20+i) // SET A, i
+		e := c.Registers()
+		e[PC] = 1
 		e[TICK] += 1
 		e[A] = uint16(i)
-		step()
-		checkRegisters(e, t)
+		c.step()
+		checkRegisters(e, c, t)
 	}
 }
 
 func TestSetAssignLiteral(t *testing.T) {
-	Reset()
-	Write(0, []uint16{0x7ff1, 0x0030}) // SET 0x1f, 0x30
-	e := Registers()
+	c := new(DCPU16)
+	c.Write(0, []uint16{0x7ff1, 0x0030}) // SET 0x1f, 0x30
+	e := c.Registers()
 	e[PC] += 2
 	e[TICK] += 3
-	step()
-	checkRegisters(e, t)
+	c.step()
+	checkRegisters(e, c, t)
 }
 
 func TestPeek(t *testing.T) {
-	Reset()
-	Write(0, []uint16{0x6401}) // SET A, PEEK
-	e := Registers()
-	e[A] = memory[0]
+	c := new(DCPU16)
+	c.Write(0, []uint16{0x6401}) // SET A, PEEK
+	e := c.Registers()
+	e[A] = c.memory[0]
 	e[PC] += 1
 	e[TICK] += 1
-	step()
-	checkRegisters(e, t)
+	c.step()
+	checkRegisters(e, c, t)
 }
 
 func TestPushPop(t *testing.T) {
-	Reset()
-	Write(0, []uint16{0x01a1, 0x6011}) // SET PUSH, A; SET B, POP
-	register[A] = 0x7f3f
-	e := Registers()
+	c := new(DCPU16)
+	c.Write(0, []uint16{0x01a1, 0x6011}) // SET PUSH, A; SET B, POP
+	c.register[A] = 0x7f3f
+	e := c.Registers()
 	e[A] = 0x7f3f
 	e[SP] = 0xffff
 	e[PC] += 1
 	e[TICK] += 1
-	step()
-	checkRegisters(e, t)
-	if memory[0xffff] != 0x7f3f {
-		t.Errorf("Expected stack to be 0x7f3f, got: %v\n", memory[0xffff])
+	c.step()
+	checkRegisters(e, c, t)
+	if c.memory[0xffff] != 0x7f3f {
+		t.Errorf("Expected stack to be 0x7f3f, got: %v\n", c.memory[0xffff])
 	}
 	e[B] = e[A]
 	e[TICK] += 1
 	e[PC] += 1
 	e[SP] = 0
-	step()
-	checkRegisters(e, t)
-	if memory[0xffff] != 0x7f3f {
-		t.Errorf("Expected stack to be 0x7f3f, got: %v\n", memory[0xffff])
+	c.step()
+	checkRegisters(e, c, t)
+	if c.memory[0xffff] != 0x7f3f {
+		t.Errorf("Expected stack to be 0x7f3f, got: %v\n", c.memory[0xffff])
 	}
 }
 
-func checkRegisters(e []uint16, t *testing.T) {
-	r := Registers()
+func TestIFG(t *testing.T) {
+	c := new(DCPU16)
+
+	// check that if A&B != 0 that pc is at next instruction
+	c.memory[0] = makeOpcode(0xf, 0, 1) // IFB A, B
+	c.register[A] = 0x7f3f
+	c.register[B] = c.register[A]
+	e := c.Registers()
+	e[A] = 0x7f3f
+	e[B] = e[A]
+	e[PC] = 1
+	e[TICK] += 2
+	c.step()
+	checkRegisters(e, c, t)
+
+	// check that if A&B == 0 that the pc is beyond next instruction, and extra cycle spent
+	c.register[B] = 0
+	c.pc = 0
+	e[B] = 0
+	e[PC] = 2
+	e[TICK] += 3
+	c.step()
+	checkRegisters(e, c, t)
+}
+
+func checkRegisters(e []uint16, c *DCPU16, t *testing.T) {
+	r := c.Registers()
 	for i, v := range r {
 		if v != e[i] {
 			t.Errorf("Expected registers to be %v, got %v\n", e, r)
